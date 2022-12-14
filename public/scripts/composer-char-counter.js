0e2d7fea8978dbd5ea823140438d2833abf3286b
Jerome <101889450+jeromealmir@users.noreply.github.com>
2022-12-14 09:46:52 -0800

    write character counter code using jQuery

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+
+  $('#tweet-text').on('keyup', function() {
+    const input = $(this).val();
+    const counter = $(this).next().children('output.counter');
+    $(counter).text(140 - input.length);
+    
+    if (input) {
+      $(counter).text(140 - input.length);
+    }
+
+    // if tweet is more than 140 characters, change text-colour to red
+    if (input.length > 140) {
+      $(counter).css("color", "red");
+    } else {
+      $(counter).css("color", "inherit");
+    }
+  })
+
+});
