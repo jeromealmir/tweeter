8d113210e9d130be0255138cd7bf7448971cbdf1
Jerome <101889450+jeromealmir@users.noreply.github.com>
2022-12-14 19:03:06 -0800

    implement a jQuery function that will fetch tweet data

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,71 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+$(document).ready(function(){
+
+  const data = [
+    {
+      "user": {
+        "name": "Newton",
+        "avatars": "https://i.imgur.com/73hZDYK.png",
+        "handle": "@SirIsaac"
+      },
+      "content": {
+        "text": "If I have seen further it is by standing on the shoulders of giants"
+      },
+      "created_at": 1461116232227
+    },
+    {
+      "user": {
+        "name": "Descartes",
+        "avatars": "https://i.imgur.com/nlhLi3I.png",
+        "handle": "@rd" },
+      "content": {
+        "text": "Je pense , donc je suis"
+      },
+      "created_at": 1461113959088
+    }
+  ]
+
+  const createTweetElement = (object, callback) => {
+    const userName = object.user && object.user.name || 'anonymous';
+    const userAvatar = object.user && object.user.avatars || 'https://i.imgur.com/gueO6ye.png';
+    const userHandle = object.user && object.user.handle || '@anonymous';
+    const tweetContent = object.content && object.content.text || 'Lorem ipsum dolor sit amet, consectetur adipiscing elit';
+    const creationDate = object['created_at'] || '0';
+
+    const tweet = `
+    <article class="tweets-container">
+        <header>
+          <img src="${userAvatar}" alt="user-avatar" width="50" height="50">
+          <span class="tweet-user-name">${userName}</span>
+          <span class="tweet-user-handle">${userHandle}</span>
+        </header>
+        <p class="tweet-body">
+        ${tweetContent}
+        </p>
+        <footer>
+          <span>${creationDate} days ago</span>
+          <span class="tweet-footer-icons">
+            <i class="fa-solid fa-flag"></i>
+            <i class="fa-solid fa-retweet"></i>
+            <i class="fa-solid fa-heart"></i>
+          </span>
+        </footer>
+      </article>
+    `;
+  
+    return tweet;
+  };
+
+  const renderTweets = (array) => {
+    for (const item of array) {
+      const $tweet = createTweetElement(item)
+      $('main').append($tweet);
+    }
+    return;
+  };
+
+  renderTweets(data);
+});
+
